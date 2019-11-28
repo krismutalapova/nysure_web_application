2b0c9c8b3630e23d463b960022a000d86f45850b
MeriM-dev <meri.vancovska@gmail.com>
2019-11-28 14:47:47 +0100

    Trello-106: Create a class for User
    Add getters and setters for address and phone.

--- src/main/java/se/kth/sda6/nysure/user/User.java
+++ src/main/java/se/kth/sda6/nysure/user/User.java
@@ -88,4 +88,20 @@ public class User {
     public void setName(String name) {
         this.name = name;
     }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
