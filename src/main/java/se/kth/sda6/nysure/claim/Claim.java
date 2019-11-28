26614ac45aa03977648168574d2d9e79fcda15ff
Kristina Mutalapova <kris.mutalapova@gmail.com>
2019-11-28 11:16:11 +0100

    Claim class modified

--- src/main/java/se/kth/sda6/nysure/claim/Claim.java
+++ src/main/java/se/kth/sda6/nysure/claim/Claim.java
@@ -1,4 +1,47 @@
 package se.kth.sda6.nysure.claim;
 
+import se.kth.sda6.nysure.user.User;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "claim")
 public class Claim {
+    @Id
+    @Column(name = "claimId")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long claimId;
+
+    @Column(name = "incidentDescription")
+    private String inDescr;
+
+    @ManyToOne()
+    private User user;
+
+    public Claim() {
+    }
+
+    public Long getClaimId() {
+        return claimId;
+    }
+
+    public void setClaimId(Long claimId) {
+        this.claimId = claimId;
+    }
+
+    public String getInDescr() {
+        return inDescr;
+    }
+
+    public void setInDescr(String inDescr) {
+        this.inDescr = inDescr;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
+
