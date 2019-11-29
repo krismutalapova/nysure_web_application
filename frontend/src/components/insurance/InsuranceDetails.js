720f6befc900f72d57e89fbe5ed75584a22282bf
Kristina Mutalapova <kris.mutalapova@gmail.com>
2019-11-29 10:52:11 +0100

    modifications added to InsuranceDetails

--- frontend/src/components/insurance/InsuranceDetails.js
+++ frontend/src/components/insurance/InsuranceDetails.js
@@ -1,12 +1,27 @@
 import React from "react";
+import InsuranceApi from "./../../api/InsuranceApi";
 
+class InsuranceDetails extends React.Component {
+    constructor(props) {
+        super(props);
 
-class InsuranceDetails {
+        this.state = {
+            insurances : []
+        };
+    }
+    componentDidMount() {
+        InsuranceApi.getAllInsurances()
+            .then(({data}) => this.setState({insurances: data}))
+            .catch(err => console.error(err));
+            
+    }
+
     render (){
+        console.log(this.state.insurances)
         return (
-
+            
             <div>Hello Gradle</div>
         )
-    };
+    }   
 }
 export default InsuranceDetails;
