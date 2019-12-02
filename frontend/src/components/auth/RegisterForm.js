8b0dc8da4bb7968d7346772fa2d7f44d1b7a415d
Alberto Martinez <alberto.martinez@gmail.com>
2019-12-02 17:49:59 +0100

    Modified the Login page and subitems to be more responsive

--- frontend/src/components/auth/RegisterForm.js
+++ frontend/src/components/auth/RegisterForm.js
@@ -1,46 +1,21 @@
 import React, {useState} from "react";
 
 function RegisterForm({onSubmit}) {
-    const [name, setName] = useState("");
     const [id, setId]=useState("");
-    const [address, setAddress]=useState("");
-    const [telephone, setTelephone]=useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
     return (
         <div className="card">
             <div className="card-body">
-                <h4 className="card-title">Register</h4>
-                <div>
                     <div className="form-group">
-                        <label>Name:</label>
-                        <input 
-                            type="text"
-                            className="form-control"
-                            value={name}
-                            onChange={ e => setName(e.target.value) }
-                            placeholder="Name"/>
-                    </div>
-
-                    <div className="form-group">
-                        <label>Id:</label>
+                        <label>Personnummer:</label>
                         <input 
                             type="text"
                             className="form-control"
                             value={id}
                             onChange={ e => setId(e.target.value) }
                             placeholder="YYYYMMDD-XXXX"/>
-                    </div>
-
-                    <div className="form-group">
-                        <label>Address:</label>
-                        <input 
-                            type="text"
-                            className="form-control"
-                            value={address}
-                            onChange={ e => setAddress(e.target.value) }
-                            placeholder="Address"/>
                     </div>
 
                     <div className="form-group">
@@ -54,16 +29,6 @@ function RegisterForm({onSubmit}) {
                     </div>
 
                     <div className="form-group">
-                        <label>Telephone:</label>
-                        <input
-                            type="text"
-                            value={telephone}
-                            onChange={ e => setTelephone(e.target.value) }
-                            className="form-control"
-                            placeholder="Telephone"/>
-                    </div>
-
-                    <div className="form-group">
                         <label>Password:</label>
                         <input 
                             type="password" 
@@ -74,14 +39,12 @@ function RegisterForm({onSubmit}) {
                     </div>
 
                     <div className="form-group">
-                        <button 
-                            className="btn btn-success"
-                            onClick={e => onSubmit({id, name, email, password, address, telephone})}>
+                        <button
+                            className="btn btn-success btn-block"
+                            onClick={e => onSubmit({id, email, password})}>
                             Create account
                         </button>
                     </div>
-
-                </div>
             </div>
         </div>
     );
