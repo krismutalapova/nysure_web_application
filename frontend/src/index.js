ef6a8839a921074304e7aea91daae72dc8959217
MeriM-dev <meri.vancovska@gmail.com>
2019-12-04 15:20:16 +0100

    reordered the imports in index.html

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
-import App from './App';
+import './../../frontend/src/App.css';
+
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
