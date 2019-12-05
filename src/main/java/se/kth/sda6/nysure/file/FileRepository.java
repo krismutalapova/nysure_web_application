40a355505cad49b823659d59a191916665982f58
mabmalia <mabmalia@gmail.com>
2019-12-05 09:37:24 +0100

    files have an item id

--- src/main/java/se/kth/sda6/nysure/file/FileRepository.java
+++ src/main/java/se/kth/sda6/nysure/file/FileRepository.java
@@ -1,6 +1,12 @@
 package se.kth.sda6.nysure.file;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import se.kth.sda6.nysure.item.Item;
+
+import java.util.List;
+
 @Repository
 public interface FileRepository extends CrudRepository<File, Long>{
+    List<File> findAllByItem(Item item);
 }
