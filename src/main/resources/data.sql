913c6ca974db5aa7bbe59962bd7e4ddd7dcc3100
mabmalia <mabmalia@gmail.com>
2019-12-09 09:50:06 +0100

    added data sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
 INSERT INTO insurance (id, type, company, cost, status) VALUES ('123456/2017', 'Travel', 'Folksam', 500, false);
-INSERT INTO insurance (id, type, company, cost, status) VALUES ('123457/2014', 'House', 'Allianz', 500, false);
-INSERT INTO insurance (id, type, company, cost, status) VALUES ('132523/2018', 'Auto', 'Folksam', 500, false);
-INSERT INTO insurance (id, type, company, cost, status) VALUES ('213435/2013', 'Auto', 'Folksam', 500, false);
-INSERT INTO insurance (id, type, company, cost, status) VALUES ('347459/2017', 'Health', 'Folksam', 500, false);
-INSERT INTO insurance (id, type, company, cost, status) VALUES ('214137/2011', 'Travel', 'Folksam', 500, false);
+INSERT INTO insurance (id, type, company, cost, status) VALUES ('123457/2014', 'House', 'Tre Kronor', 500, false);
+INSERT INTO insurance (id, type, company, cost, status) VALUES ('132523/2018', 'Auto', 'Bliwa', 500, false);
+INSERT INTO insurance (id, type, company, cost, status) VALUES ('213435/2013', 'Auto', 'Dina Försäkringar', 500, false);
+INSERT INTO insurance (id, type, company, cost, status) VALUES ('347459/2017', 'Health', 'Gjensidige', 500, false);
+INSERT INTO insurance (id, type, company, cost, status) VALUES ('214137/2011', 'Travel', 'Länsförsäkringar', 500, false);
