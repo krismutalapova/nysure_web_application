f2e274a02d54c593e978903ab8da12a2a7acfa93
mabmalia <mabmalia@gmail.com>
2019-12-09 14:18:08 +0100

    updated data sql file

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,7 @@
-INSERT INTO insurance (id, type, company, cost, status) VALUES ('123456/2017', 'Travel', 'Folksam', 500, false);
-INSERT INTO insurance (id, type, company, cost, status) VALUES ('123457/2014', 'House', 'Tre Kronor', 500, false);
-INSERT INTO insurance (id, type, company, cost, status) VALUES ('132523/2018', 'Auto', 'Bliwa', 500, false);
-INSERT INTO insurance (id, type, company, cost, status) VALUES ('213435/2013', 'Auto', 'Dina Försäkringar', 500, false);
-INSERT INTO insurance (id, type, company, cost, status) VALUES ('347459/2017', 'Health', 'Gjensidige', 500, false);
-INSERT INTO insurance (id, type, company, cost, status) VALUES ('214137/2011', 'Travel', 'Länsförsäkringar', 500, false);
+INSERT INTO account VALUES ('19841019-1234', 'm@gmail.com', 'Miguel Angelo', '$2a$10$6vsIkS2R4JRSjEjpOKo5l.3Sc33piuNrdm9W75L6pWcuc21.u.IBi', '0700123456');
+INSERT INTO insurance VALUES ('123456/2017', 'Folksam', 1000, false, 'Travel', null);
+INSERT INTO insurance VALUES ('123457/2014', 'Tre Kronor', 699, false, 'House',null);
+INSERT INTO insurance VALUES ('132523/2018','Bliwa', 599, false, 'Auto', null);
+INSERT INTO insurance VALUES ('213435/2013', 'Dina Försäkringar', 1299, false, 'Auto', null);
+INSERT INTO insurance VALUES ('347459/2017', 'Gjensidige', 599, false, 'Health', null);
+INSERT INTO insurance VALUES ('214137/2011', 'Länsförsäkringar', 749, false, 'Travel', null);
