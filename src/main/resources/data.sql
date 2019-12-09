46f7d8c1ae76e15f07fbd3cfd36793684c25c50b
mabmalia <mabmalia@gmail.com>
2019-12-09 09:23:52 +0100

    add data sql file

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO insurance (policy_id, type, amount, company, status) VALUES (123455, 'House', 500, 'Folksam', false);
+INSERT INTO insurance (policy_id, type, amount, company, status) VALUES (123455, 'House', 500, 'Folksam', false);
+INSERT INTO insurance (policy_id, type, amount, company, status) VALUES (123455, 'House', 500, 'Folksam', false);
