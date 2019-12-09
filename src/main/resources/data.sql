2c0430af25c89f3d601087cb791d8cedd6ff3793
mabmalia <mabmalia@gmail.com>
2019-12-09 09:40:08 +0100

    updated data sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,6 @@
-INSERT INTO insurance (policy_id, type, amount, company, status) VALUES (123455, 'House', 500, 'Folksam', false);
-INSERT INTO insurance (policy_id, type, amount, company, status) VALUES (123455, 'House', 500, 'Folksam', false);
-INSERT INTO insurance (policy_id, type, amount, company, status) VALUES (123455, 'House', 500, 'Folksam', false);
+INSERT INTO insurance (id, type, company, cost, status) VALUES ('123456/2017', 'Travel', 'Folksam', 500, false);
+INSERT INTO insurance (id, type, company, cost, status) VALUES ('123457/2014', 'House', 'Allianz', 500, false);
+INSERT INTO insurance (id, type, company, cost, status) VALUES ('132523/2018', 'Auto', 'Folksam', 500, false);
+INSERT INTO insurance (id, type, company, cost, status) VALUES ('213435/2013', 'Auto', 'Folksam', 500, false);
+INSERT INTO insurance (id, type, company, cost, status) VALUES ('347459/2017', 'Health', 'Folksam', 500, false);
+INSERT INTO insurance (id, type, company, cost, status) VALUES ('214137/2011', 'Travel', 'Folksam', 500, false);
