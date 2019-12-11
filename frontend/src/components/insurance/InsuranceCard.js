37f201287e3f747bf989e7eaf81e2dddb8583269
Kristina Mutalapova <kris.mutalapova@gmail.com>
2019-12-11 15:39:56 +0100

    delete button added

--- frontend/src/components/insurance/InsuranceCard.js
+++ frontend/src/components/insurance/InsuranceCard.js
@@ -1,14 +1,18 @@
 import React, { Component } from "react";
-import{ uploadInsuranceFile, getAllInsuranceFiles } from "./../../api/FileApi";
+import { uploadInsuranceFile, getAllInsuranceFiles } from "./../../api/FileApi";
 
 
 class InsuranceCard extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
         this.state = {
             selectedFiles: [],
         }
     };
+
+    handleDelete(id) { 
+        console.log(id);
+    }
 
     onInsuranceFileChangeHandler = (e) => {
         e.preventDefault();
@@ -20,34 +24,34 @@ class InsuranceCard extends Component {
 
         uploadInsuranceFile(this.props.insurance.id, formData)
             .then(res => {
-                    const selectedFiles = this.state.selectedFiles.concat(res.data.data);
-                    this.setState(
-                        { selectedFiles }
-                    );
+                const selectedFiles = this.state.selectedFiles.concat(res.data.data);
+                this.setState(
+                    { selectedFiles }
+                );
             })
             .catch(err => console.error(err));
     };
-    
+
 
     componentDidMount() {
         //get all files
         getAllInsuranceFiles(this.props.insurance.id)
-           .then(({ data }) => this.setState({ selectedFiles: data }))
-           .catch(err => console.error(err));
+            .then(({ data }) => this.setState({ selectedFiles: data }))
+            .catch(err => console.error(err));
     }
 
     render() {
-        const {selectedFiles} = this.state;
+        const { selectedFiles } = this.state;
         const uploadedImages = selectedFiles.filter(image => image.fileType.includes("image"));
         const uploadedDocuments = selectedFiles.filter(image => !image.fileType.includes("image"));
 
 
-        if (uploadedImages.length>0){
-           uploadedImages[0].isActive=true
+        if (uploadedImages.length > 0) {
+            uploadedImages[0].isActive = true
         }
         return (
             <div className="card" style={cardStyle}>
-            <div className="card-body" >
+                <div className="card-body" >
                     <label htmlFor="type"> Policy ID:</label>
                     <input disabled type="text" className="form-control" id="usr" style={selectStyle} value={this.props.insurance.id}></input>
 
@@ -62,88 +66,94 @@ class InsuranceCard extends Component {
 
                     <div className="form-group files color" style={selectStyle}>
                         <label>Upload Your File</label>
-                        <input type="file" className="form-control" onChange={this.onInsuranceFileChangeHandler}/>
+                        <input type="file" className="form-control" onChange={this.onInsuranceFileChangeHandler} />
                     </div>
 
                     {
-                            uploadedDocuments.length > 0
-                            ? <DownloadDocs uploadedDocuments={uploadedDocuments} />
+                        uploadedDocuments.length > 0
+                            ? <DownloadDocs uploadedDocuments={uploadedDocuments} handleDelete={(id) => this.handleDelete(id)}/>
                             : ""
-                        }
-                        {
-                            uploadedImages.length > 0
+                    }
+                    {
+                        uploadedImages.length > 0
                             ? <Carousel uploadedImages={uploadedImages} />
                             : ""
-                        }
-                        </div>
+                    }
+                </div>
             </div>
         )
     };
 }
-                    const DownloadDocs = ({uploadedDocuments}) =>
-                        <div id="listOfDocuments">
-                            <label htmlFor="uploadDoc"><b>Insurance documents:</b></label>
-                            <small className="text-muted float-right">{`${uploadedDocuments.length} document${uploadedDocuments.length === 1 ? "" : "s"} uploaded`}</small>
-                                {uploadedDocuments.map(({ id, fileName, fileType, fileData, }) => {
-                                                return (
-                                                    <p key={id}>
-                                                            {fileName} : <a className="fa fa-download float-right"
-                                                                            download
-                                                                            href={
-                                                                            "data:" + fileType +
-                                                                            ";base64," + fileData }></a>
-                                                                        <a className="fa fa-trash-alt"
-                                                                            erase
-                                                                            href={
-                                                                            "data:" + fileType +
-                                                                            ";base64," + fileData }>delete</a>
-                                                     </p>
-                                                )
-                                            })
-                                }
-                    </div>
 
-                    const Carousel = ({uploadedImages}) =>
-                        <div>
-                        <label htmlFor="uploadPhoto"><b>Insurance photos:</b></label>
-                        <small className="text-muted float-right">{`${uploadedImages.length} photo${uploadedImages.length === 1 ? "" : "s"} uploaded`}</small>
-                        <div id="carouselItem" className="carousel slide" data-ride="carousel">
-                            <div className="carousel-inner">
-                                {uploadedImages.map(({ id, fileName, fileType, fileData, isActive }) => {
-                                    return (
-                                        <div key={id} className={"carousel-item" + (isActive ? " active" : "")}>
-                                            <img className="d-block w-100"
-                                                src={
-                                                    "data:" + fileType +
-                                                    ";base64," + fileData
-                                                } alt={fileName} />
-                                        </div>
-                                    )
-                                })
-                                }
-                        
-                        <a className="carousel-control-prev" href="#carouselInsurance" role="button" data-slide="prev">
-                            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span className="sr-only">Previous</span>
-                        </a>
-                        <a className="carousel-control-next" href="#carouselInsurance" role="button" data-slide="next">
-                            <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span className="sr-only">Next</span>
-                        </a>
-                            </div>
+const DownloadDocs = ({ uploadedDocuments, handleDelete }) =>
+    <div id="listOfDocuments">
+        <label htmlFor="uploadDoc"><b>Insurance documents:</b></label>
+        <small className="text-muted float-right">{`${uploadedDocuments.length} document${uploadedDocuments.length === 1 ? "" : "s"} uploaded`}</small>
+        {uploadedDocuments.map(({ id, fileName, fileType, fileData, }) => {
+            return (
+                <p key={id}>
+                    {fileName} :
+                    <a className="fa fa-trash float-right"
+                        style={buttonStyle}
+                        href="#"
+                        onClick={() => handleDelete(id)}
+                    ></a>
+                    <a className="fa fa-download float-right"
+                        style={buttonStyle}
+                        download
+                        href={
+                            "data:" + fileType +
+                            ";base64," + fileData}></a>
+                </p>
+            )
+        })
+        }
+    </div>
+
+const Carousel = ({ uploadedImages }) =>
+    <div>
+        <label htmlFor="uploadPhoto"><b>Insurance photos:</b></label>
+        <small className="text-muted float-right">{`${uploadedImages.length} photo${uploadedImages.length === 1 ? "" : "s"} uploaded`}</small>
+        <div id="carouselItem" className="carousel slide" data-ride="carousel">
+            <div className="carousel-inner">
+                {uploadedImages.map(({ id, fileName, fileType, fileData, isActive }) => {
+                    return (
+                        <div key={id} className={"carousel-item" + (isActive ? " active" : "")}>
+                            <img className="d-block w-100"
+                                src={
+                                    "data:" + fileType +
+                                    ";base64," + fileData
+                                } alt={fileName} />
                         </div>
-                        </div>
+                    )
+                })
+                }
 
-const selectStyle ={
-   width: '100%',
-   margin: 'auto',
-   marginBottom: '10px'
-  }
+                <a className="carousel-control-prev" href="#carouselInsurance" role="button" data-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="sr-only">Previous</span>
+                </a>
+                <a className="carousel-control-next" href="#carouselInsurance" role="button" data-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="sr-only">Next</span>
+                </a>
+            </div>
+        </div>
+    </div>
 
+const selectStyle = {
+    width: '100%',
+    margin: 'auto',
+    marginBottom: '10px'
+}
+const buttonStyle = {
+    marginRight: '15px',
+
+}
 const cardStyle = {
     width: '80%',
     margin: 'auto'
-  }
-  
+}
+
 
 export default InsuranceCard;
