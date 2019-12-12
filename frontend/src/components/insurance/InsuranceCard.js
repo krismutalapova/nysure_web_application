73b8075101cb293ff117ae33a462193d9c1ee45e
mabmalia <mabmalia@gmail.com>
2019-12-12 16:06:34 +0100

    fixed issue of repeated element ids

--- frontend/src/components/insurance/InsuranceCard.js
+++ frontend/src/components/insurance/InsuranceCard.js
@@ -54,16 +54,16 @@ class InsuranceCard extends Component {
             <div className="card" style={cardStyle}>
                 <div className="card-body" >
                     <label htmlFor="type"> Policy ID:</label>
-                    <input disabled type="text" className="form-control" id="usr" style={selectStyle} value={this.props.insurance.id}></input>
+                    <input disabled type="text" className="form-control" style={selectStyle} value={this.props.insurance.id}></input>
 
                     <label htmlFor="type">Company: </label>
-                    <input disabled type="text" className="form-control" id="usr" style={selectStyle} value={this.props.insurance.company}></input>
+                    <input disabled type="text" className="form-control" style={selectStyle} value={this.props.insurance.company}></input>
 
                     <label htmlFor="type">Cost: </label>
-                    <input disabled type="text" className="form-control" id="usr" style={selectStyle} value={this.props.insurance.cost}></input>
+                    <input disabled type="text" className="form-control" style={selectStyle} value={this.props.insurance.cost}></input>
 
                     <label htmlFor="type">Type: </label>
-                    <input disabled type="text" className="form-control" id="usr" style={selectStyle} value={this.props.insurance.type}></input>
+                    <input disabled type="text" className="form-control" style={selectStyle} value={this.props.insurance.type}></input>
 
                     <div className="form-group files color" style={selectStyle}>
                         <label>Upload Your File</label>
