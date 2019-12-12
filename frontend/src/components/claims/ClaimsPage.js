afe88badf937d010e20aa5424462e0df7e67635b
Pinelopi85 <44837154+Pinelopi85@users.noreply.github.com>
2019-12-12 17:38:57 +0100

    Claims layout (#59)
    * added claims table
    
    * added claims table
    
    * added style and headers
    
    * added claims table in separate page
    
    * fixed table layout
    
    * fixed table layout
    
    * amended layout
    
    * cleaned code
    
    * changed table values
    
    *  added card with dropdown
    
    * minor modifications
    
    * adding modal
    
    * added modal and changed layout
    
    * changed layout
    
    * added components to claims
    
    * added styling
    
    * created claims with buttons
    
    * discovered problem in href

---
+++ frontend/src/components/claims/ClaimsPage.js
@@ -0,0 +1,122 @@
+import React, { Component } from "react";
+import ClaimsForm from "./ClaimsForm";
+import ClaimsApi from "../../api/ClaimsApi";
+import ClaimsSelection from "./ClaimsSelection";
+
+class ClaimsPage extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            claims: [],
+        }
+    }
+
+    async onClickCreateClaims(claimsData) {
+        try {
+            const response = await ClaimsApi.changeStatus(claimsData);
+            const claim = response.data;
+            if (claim !== "") {
+                const newclaim = this.state.claims.concat(claim);
+                this.setState({
+                claims: newclaim,
+            });}
+        }
+        catch (e) {
+            console.error(e);
+        }
+    }
+
+    componentDidMount() {
+        //get all claims by status
+        ClaimsApi.getAllByStatus("true")
+            .then(({ data }) => this.setState({ claims: data }))
+            .catch(err => console.error(err));
+    }
+
+    render() {
+        const { claims } = this.state;
+        console.log(claims);
+
+        return (
+            <div id="main-group">
+                <div className="row">
+                    <button type="button"
+                        className="btn btn-outline-secondary btn-lg"
+                        style={buttonStyle}
+                        data-toggle="modal"
+                        data-target={"#claimFormModal"}> <i className="fa fa-plus"></i>
+                    </button>
+
+                    <button type="submit" disabled
+                        className="btn btn-lg font-weight-bold"
+                        style={buttonStyle}>Add a claim </button>
+                        <Modal id="claimFormModal" title="Choose type of insurance">
+                                <ClaimsSelection />
+                        </Modal>
+                </div>
+                <div className="row">
+                    {claims.map(( claim ) => {
+                        return (
+                            <div key={claim.id} className="card" style={cardStyle}>
+                                <div className="card-body">
+                                    <h5 className="card-title">{claim.type}</h5>
+                                    <p className="card-text">{claim.company}</p>
+                                    <p className="card-text">{claim.cost} SEK</p>
+                                    <button type="button"
+                                        className="btn btn-primary"
+                                        data-toggle="modal"
+                                        data-target={`#claimCardModal-${claim.id}`}> View claim
+                                        </button>
+                                </div>
+                                <Modal id={`claimCardModal-${claim.id}`} title={claim.type}>
+                                     <claimCard claim={claim}/>
+                                </Modal>
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
+        );
+    }
+}
+const Modal = ({ children, id, title }) =>
+    <div id={id} className="modal fade" role="dialog">
+        <div className="modal-dialog modal-xl" style={modalBorderStyle}>
+            <div className="modal-content" style={modalBorderStyle}>
+                <div className="modal-header">
+                    <h4 className="modal-title">{title}</h4>
+                    <button type="button" className="close" data-dismiss="modal"> &times;
+                                                    </button>
+                </div>
+                <div className="modal-body">
+                    {children}
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+const cardStyle = {
+    width: '100%',
+    maxWidth: '400px',
+    margin: '10px',
+}
+
+const buttonStyle = {
+    marginRight: '10px',
+    marginBottom: '10px',
+}
+
+const modalBorderStyle = {
+    borderRadius: '10px',
+    width: 'auto', 
+  }
+
+  const modalForm= {
+      width: '100%', 
+  }
+
+
+
+export default ClaimsPage;
--- frontend/src/components/claims/ClaimsSelection.js
+++ frontend/src/components/claims/ClaimsSelection.js
@@ -1,65 +1,60 @@
 import React, { Component } from "react";
 
-class Quota extends Component {
-
+class ClaimsSelection extends Component {
     render() {
         return (
             <div>
                 <div className="row-100">
                     <div className="col-4 center-content">
-                        <a href="/quote/form/vehicle"> //add correct href for claims folder
-                            <img className="btn-circle btn-circle-lg" src="/images/car2.png" alt="car2" />
+                        <a href="/claims//vehicle">
+                            <img className="btn-circle btn-circle" src="/images/car2.png" alt="car2" />
                         </a>
                         <p>Vehicle</p>
                     </div>
                     <div className="col-4 center-content">
-                        <a href="/quote/form/home">
-                        <img className="btn-circle btn-circle-lg" src="/images/home2.png" alt="home2" />
+                        <a href="/claims/form/home">
+                        <img className="btn-circle btn-circle" src="/images/home2.png" alt="home2" />
                         </a>
                         <p>Home</p>
                     </div>
                     <div className="col-4 center-content">
-                        <a href="/quote/form/child">
-                        <img className="btn-circle btn-circle-lg" src="/images/children.png" alt="children" />
+                        <a href="/claims/form/child">
+                        <img className="btn-circle btn-circle" src="/images/children.png" alt="children" />
                         </a>
                         <p>Child</p>              
                     </div>
                     <div className="col-4 center-content">
-                        <a href="/quote/form/pet">
-                        <img className="btn-circle btn-circle-lg" src="/images/pet.png" alt="pet" />
+                        <a href="/claims/form/pet">
+                        <img className="btn-circle btn-circle" src="/images/pet.png" alt="pet" />
                         </a>
                         <p>Pet</p>
                     </div>
                 </div>
                 <div className="row-100">
                     <div className="col-4 center-content">
-                        <a href="/quote/form/travel">
-                        <img className="btn-circle btn-circle-lg" src="/images/plane2.png" alt="plane2" />
+                        <a href="/claims/form/travel">
+                        <img className="btn-circle btn-circle" src="/images/plane2.png" alt="plane2" />
                         </a>
                         <p>Travel</p>
                     </div>
                     <div className="col-4 center-content">
-                        <a href="/quote/form/life">
-                        <img className="btn-circle btn-circle-lg" src="/images/life.png" alt="life" />
+                        <a href="/claims/claimsform/li">
+                        <img className="btn-circle btn-circle" src="/images/life.png" alt="life" />
                         </a>
                         <p>Life</p>                   
                     </div>
                     <div className="col-4 center-content">
-                        <a href="/quote/form/health">
-                        <img className="btn-circle btn-circle-lg" src="/images/health.png" alt="health" />
+                        <a href="/claims/form/health">
+                        <img className="btn-circle btn-circle" src="/images/health.png" alt="health" />
                         </a>
                         <p>Health</p>
                     </div>
                     <div className="col-4 center-content">
-                        <a href="/quote/form/business">
-                        <img className="btn-circle btn-circle-lg" src="/images/briefcase2.png" alt="briefcase2" />
+                        <a href="/claims/form/business">
+                        <img className="btn-circle btn-circle" src="/images/briefcase2.png" alt="briefcase2" />
                         </a>
                         <p>Business</p>
                     </div>
-                </div>
-
-                <div className="row-full">
-                    <div className="bottomPadding"></div>
                 </div>
             </div>
         )
@@ -67,4 +62,4 @@ class Quota extends Component {
 
 }
 
-export default Quota;
+export default ClaimsSelection;
