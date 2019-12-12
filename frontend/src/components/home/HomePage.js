53daeb37a92a29e2b641e82f012e4b50d99d2238
mabmalia <mabmalia@gmail.com>
2019-12-13 00:05:49 +0100

    implemented balance in the homepage

--- frontend/src/components/home/HomePage.js
+++ frontend/src/components/home/HomePage.js
@@ -1,17 +1,41 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import InsuranceApi from "../../api/InsuranceApi";
+import UserApi from "../../api/UserApi";
 
 
-function HomePage() {
+class HomePage extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            balance: 0,
+        }
+    }
+
+    numberWithCommas() {
+        return this.state.balance.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+
+    componentDidMount() {
+        //get the total cost of insurances
+        UserApi.current()
+            .then((res) => {
+                return InsuranceApi.getSumByUser(res.data.id)})
+            .then((res) => {
+                this.setState({ balance: res.data })})
+            .catch(err => console.error(err));
+    }
+
+    render() {
         return (
             <div>
                 <div className="column">
                     <div className="row-full">
                         <div className="column">
                             <div className="card text-white estimated-spent">
-                                <h4 className="">Estimated cost:</h4>
-                                <br />
-                                <p className="">SEK 122,334,454.00</p>
+                                <h4 className="">Estimated monthly cost:</h4>
+                                <h5 className="">{`${this.numberWithCommas()} SEK per month`}</h5>
                             </div>
                         </div>
                     </div>
@@ -19,7 +43,7 @@ function HomePage() {
                 <div className="column">
                     <div className="row-full">
                         <div className="col-4 center-content">
-                        <Link to="/item" className="nav-link">
+                            <Link to="/item" className="nav-link">
                                 <img className="btn-circle btn-circle-lg" src="/images/photo.png " alt="item" />
                             </Link>
                             <p>Items</p>
@@ -49,6 +73,7 @@ function HomePage() {
                 </div>
             </div>
         );
+    };
 }
 
 export default HomePage;
