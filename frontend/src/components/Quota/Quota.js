fb6882df10316bd2e56b573c3e7872e51695c277
Kristina Mutalapova <kris.mutalapova@gmail.com>
2019-12-13 11:31:30 +0100

    scalability to homepage and quota page added

--- frontend/src/components/Quota/Quota.js
+++ frontend/src/components/Quota/Quota.js
@@ -5,52 +5,52 @@ class Quota extends Component {
     render() {
         return (
             <div>
-                <div className="row-100">
-                    <div className="col-4 center-content">
+                <div className="row">
+                    <div className="col center-content">
                         <a href="/quote/form/vehicle">
                             <img className="btn-circle btn-circle-lg" src="/images/car2.png" alt="car2" />
                         </a>
                         <p>Vehicle</p>
                     </div>
-                    <div className="col-4 center-content">
+                    <div className="col center-content">
                         <a href="/quote/form/home">
                         <img className="btn-circle btn-circle-lg" src="/images/home2.png" alt="home2" />
                         </a>
                         <p>Home</p>
                     </div>
-                    <div className="col-4 center-content">
+                    <div className="col center-content">
                         <a href="/quote/form/child">
                         <img className="btn-circle btn-circle-lg" src="/images/children.png" alt="children" />
                         </a>
                         <p>Child</p>              
                     </div>
-                    <div className="col-4 center-content">
+                    <div className="col center-content">
                         <a href="/quote/form/pet">
                         <img className="btn-circle btn-circle-lg" src="/images/pet.png" alt="pet" />
                         </a>
                         <p>Pet</p>
                     </div>
                 </div>
-                <div className="row-100">
-                    <div className="col-4 center-content">
+                <div className="row">
+                    <div className="col center-content">
                         <a href="/quote/form/travel">
                         <img className="btn-circle btn-circle-lg" src="/images/plane2.png" alt="plane2" />
                         </a>
                         <p>Travel</p>
                     </div>
-                    <div className="col-4 center-content">
+                    <div className="col center-content">
                         <a href="/quote/form/life">
                         <img className="btn-circle btn-circle-lg" src="/images/life.png" alt="life" />
                         </a>
                         <p>Life</p>                   
                     </div>
-                    <div className="col-4 center-content">
+                    <div className="col center-content">
                         <a href="/quote/form/health">
                         <img className="btn-circle btn-circle-lg" src="/images/health.png" alt="health" />
                         </a>
                         <p>Health</p>
                     </div>
-                    <div className="col-4 center-content">
+                    <div className="col center-content">
                         <a href="/quote/form/business">
                         <img className="btn-circle btn-circle-lg" src="/images/briefcase2.png" alt="briefcase2" />
                         </a>
