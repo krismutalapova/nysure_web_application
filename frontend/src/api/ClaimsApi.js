a3963ef787c940123bdf41401c24aecf23a74d06
mabmalia <mabmalia@gmail.com>
2019-12-14 02:47:57 +0100

    reverting changes in ClaimsApi

---
+++ frontend/src/api/ClaimsApi.js
@@ -0,0 +1,33 @@
+import Api from "./Api";
+
+class ClaimsApi {
+    getAllClaims() {
+        return Api.get('/claims');
+    }
+
+    getAllById(id) {
+        return Api.get('/claims/'+id);
+    }
+
+    getAllClaimsByUser(id) {
+        return Api.get('/claims/user/'+id);
+    }
+    
+    createClaims(claims) {
+        return Api.post('/claims', claims);
+    }
+
+    updateClaims(claims) {
+        return Api.put('/claims', claims);
+    }
+
+    changeStatus(company){
+        return Api.put('/claims/change_status?company=' + company);
+    }
+
+    deleteClaims(id) {
+        return Api.delete('/claims/'+id);
+    } 
+}
+
+export default new ClaimsApi();
