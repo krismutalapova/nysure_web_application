eebb7aca43e89c105637eefe22a27f1a021e6fc5
mabmalia <mabmalia@gmail.com>
2019-12-14 04:32:37 +0100

    changed quote modal

--- frontend/src/components/Quota/Form.js
+++ frontend/src/components/Quota/Form.js
@@ -21,27 +21,17 @@ function QuotaForm(props) {
                 data-target="#myModal">
                 Submit
             </button>
-            <div id="myModal" className="wrapper modal fade" role="dialog">
-                <div className="container">
-                    <div className="row mt-4">
-                            <div className="card-body">
-                                    <div className="modal-dialog"> 
-                                        <div className="modal-content">
-                                            <div className="modal-header">
-                                                <h4 className="card-title">Calculated quote</h4>
-                                                <button
-                                                    type="button"
-                                                    className="close"
-                                                    data-dismiss="modal">
-                                                    &times;
-                                                </button>
-                                            </div>
-                                            <div className="modal-body">
-                                                {calculated(type)} Kr
-                                            </div>
-                                        </div>
-                                    </div>
-                            </div>
+            <div id="myModal" className="modal fade" role="dialog">
+                <div className="modal-dialog" style={{borderRadius: '10px'}}>
+                    <div className="modal-content" style={{borderRadius: '10px'}}>
+                        <div className="modal-header">
+                            <h4 className="modal-title">Calculated quote:</h4>
+                            <button type="button" className="close" data-dismiss="modal"> &times;
+                            </button>
+                        </div>
+                        <div className="modal-body">
+                            {calculated(type)} Kr
+                        </div>
                     </div>
                 </div>
             </div>
