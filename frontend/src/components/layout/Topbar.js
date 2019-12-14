27fafdc80299eecf97fc0667ca9225893d5e5a3f
Alberto Martinez <alberto.martinez@gmail.com>
2019-12-14 20:12:30 +0100

    TopBar and NavBar styling changes

--- frontend/src/components/layout/Topbar.js
+++ frontend/src/components/layout/Topbar.js
@@ -1,18 +1,31 @@
 import React from 'react';
 
-function Topbar({onLogout, user}) {
+function Topbar({ onLogout, user }) {
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark topBar">
-            <a className="navbar-brand" href="/">
-                <img src="/images/NySure-Logo-96.png" alt="logo" />
+            <a className="navbar-brand" href="/" alt="Home">
+                <img src="/images/NySureLogoWhite.png" style={{ width: "140px", height: "auto" }} alt="logo" />
             </a>
             <div className="collapse navbar-collapse" id="navbarColor01">
                 <p className="user">
-                Signed in as {user.email || 'N/A'}
-                    <a href="/user">
-                        <span className="edit-info"></span>
-                    </a>
+                    Welcome  <strong style={{fontSize: "25px"}}> {user.name || user.email } ,</strong>
+                    <br></br>
+                    what would you like to keep safe today?
+
                 </p>
+            </div>
+            <div class="userContainer">
+                <a href="/profile">
+                    <img 
+                        className="profileIcon" 
+                        src="icons/user-circle-solid.png" 
+                        style={{ width: "40px", height: "40px" }} 
+                        alt="Edit User"
+                    />
+                    <div class="middle">
+                        <div class="text">Edit User</div>
+                    </div>
+                </a>
             </div>
             <button className="btn btn-outline-info my-2 my-sm-0" onClick={onLogout}>Logout</button>
         </nav>
