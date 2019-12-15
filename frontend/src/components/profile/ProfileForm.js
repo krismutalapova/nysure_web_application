c85e1b31581371e66438719db7e10b9b06c47593
mabmalia <mabmalia@gmail.com>
2019-12-15 13:07:25 +0100

    added more fields

--- frontend/src/components/profile/ProfileForm.js
+++ frontend/src/components/profile/ProfileForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import UserApi from "../../api/UserApi";
 
 
@@ -7,6 +7,8 @@ class ProfileForm extends React.Component {
 
     state = {
         name: "",
+        email: "",
+        phone: "",
         password: "",
     }
 
@@ -14,15 +16,17 @@ class ProfileForm extends React.Component {
         window.setTimeout(() => {
             this.setState({
                 name: this.props.user.name,
+                email: this.props.user.email,
+                phone: this.props.user.phone,
                 password: "",
             })
         }, 400)
-
     }
 
     handleSubmit() {
         const updatedUser = { ...this.props.user }
         updatedUser.name = this.state.name
+        updatedUser.phone = this.state.phone
         updatedUser.password = this.state.password.length ? this.state.password : this.props.user.password
         UserApi.updateUser(updatedUser)
             .then(() => {
@@ -31,44 +35,86 @@ class ProfileForm extends React.Component {
     }
 
     render() {
-
         return (
             <div className="card">
-                <div className="card-body">
+                <div className="card-body" style={{ borderRadius: '0px' }}>
                     <label>Username:</label>
-                    <div className="form-group">
+                    <div className="input-group">
                         <input
                             type="text"
-                            placeholder="What shall we call you?" 
+                            placeholder="What shall we call you?"
                             className="form-control"
+                            style={{ borderRadius: '10px' }}
                             value={this.state.name}
                             onChange={e => this.setState({ name: e.target.value })}
-                        />      
+                        />
+                        <button
+                            type="button"
+                            className="btn savebtn"
+                            onClick={e => this.handleSubmit()}>
+                            <img className="savebtn" src="/icons/save-solid.png" alt="save" />
+                        </button>
                     </div>
-                    <button
-                        type="button"
-                        className="btn savebtn"
-                        onClick={e => this.handleSubmit()}>
-                        <img className="savebtn" src="/icons/save-solid.png" alt="save" />
-                    </button>
                 </div>
 
-                <div className="card-body">
+                <div className="card-body" style={{ borderRadius: '0px' }}>
+                    <label>Email:</label>
+                    <div className="input-group">
+                        <input
+                            disabled
+                            type="text"
+                            placeholder="What is your e-mail?"
+                            className="form-control"
+                            style={{ borderRadius: '10px' }}
+                            value={this.state.email}
+                            onChange={e => this.setState({ email: e.target.value })}
+                        />
+                        <button
+                            disabled
+                            type="button"
+                            className="btn savebtn"
+                            onClick={e => this.handleSubmit()}>
+                            <img className="savebtn" src="/icons/save-solid.png" alt="save" />
+                        </button>
+                    </div>
+                </div>
+
+                <div className="card-body" style={{ borderRadius: '0px' }}>
+                    <label>Phone:</label>
+                    <div className="input-group">
+                        <input
+                            type="number"
+                            placeholder="What is your phone number?"
+                            className="form-control"
+                            style={{ borderRadius: '10px' }}
+                            value={this.state.phone}
+                            onChange={e => this.setState({ phone: e.target.value })}
+                        />
+                        <button type="button"
+                            className="btn savebtn"
+                            onClick={e => this.handleSubmit()}>
+                            <img className="savebtn" src="/icons/save-solid.png" alt="save" />
+                        </button>
+                    </div>
+                </div>
+
+                <div className="card-body" style={{ borderRadius: '0px' }}>
                     <label>Password:</label>
-                    <div className="form-group">
+                    <div className="input-group">
                         <input
                             type="password"
                             placeholder="Password"
                             className="form-control"
+                            style={{ borderRadius: '10px' }}
                             value={this.state.password}
                             onChange={e => this.setState({ password: e.target.value })}
                         />
+                        <button type="button"
+                            className="btn savebtn"
+                            onClick={e => this.handleSubmit()}>
+                            <img className="savebtn" src="/icons/save-solid.png" alt="save" />
+                        </button>
                     </div>
-                    <button type="button"
-                        className="btn savebtn"
-                        onClick={e => this.handleSubmit()}>
-                        <img className="savebtn" src="/icons/save-solid.png" alt="save" />
-                    </button>
                 </div>
             </div>
 
